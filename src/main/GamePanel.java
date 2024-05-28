10e763628bc1f03151708c8c5d92abe755b975bf
Morneee <129755978+Morneee@users.noreply.github.com>
2024-05-28 15:04:31 +0200

    changement

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -43,7 +43,8 @@ public class GamePanel extends JPanel implements Runnable{
 	KeyHandler m_keyH;
 	Thread m_gameThread;
 	Player m_player;
-	List<pnj> m_tab_pnj = new ArrayList<>();
+	List<pnj> m_tab_pnj_1 = new ArrayList<>();
+	List<pnj> m_tab_pnj_2 = new ArrayList<>();
 	List<coins> m_tab_coins = new ArrayList<>();
 	List<Craie> m_tab_craies;
 	Craie m_craie;
@@ -66,7 +67,7 @@ public class GamePanel extends JPanel implements Runnable{
 		m_tab_craies.add(m_craie);
 		m_tileM = new TileManager(this);
 		
-		entity.pnj.add_pnj_to_panel(this,m_tab_pnj);
+		entity.pnj.add_pnj_to_panel(this,m_tab_pnj_1,m_tab_pnj_2);
 		
 		entity.coins.create_tab_coordonnees();
 		entity.coins.add_Coins_to_panel(this,m_tab_coins);
@@ -233,7 +234,7 @@ public class GamePanel extends JPanel implements Runnable{
 		DialoguePNJ(g2);
 
 		if (m_tileM.m_mapChoose == 1) {
-			for (pnj pnj:m_tab_pnj) {
+			for (pnj pnj:m_tab_pnj_1) {
 				pnj.draw(g2);
 			}
 			for (coins coin:m_tab_coins) {
@@ -244,6 +245,9 @@ public class GamePanel extends JPanel implements Runnable{
 			for (Craie craie : m_tab_craies) {
 				craie.draw(g2);
 		    }
+			for (pnj pnj:m_tab_pnj_2) {
+				pnj.draw(g2);
+			}
 		}
 		
 		collectCraie();
@@ -278,10 +282,10 @@ public class GamePanel extends JPanel implements Runnable{
 		g2.setColor(Color.BLACK);
         g2.setFont(new Font("Arial", Font.BOLD, 12));
         
-		if (m_player.checkCollision(m_tab_pnj.get(0).m_x, m_tab_pnj.get(0).m_y, TILE_SIZE)) {
+		if (m_player.checkCollision(m_tab_pnj_1.get(0).m_x, m_tab_pnj_1.get(0).m_y, TILE_SIZE)) {
 			boolean var = true;
 			if (var) {
-				g2.drawString("Tu peux aller me chercher une craie dans la salle 003 ?", m_player.m_x, m_player.m_y - 10);
+				g2.drawString("Tu peux aller me chercher une craie dans la salle 103 ?", m_player.m_x, m_player.m_y - 10);
 			}
 			if (m_tileM.m_use && inventaire.contains(m_craie) ) {
 				inventaire.remove(m_craie);
@@ -291,11 +295,15 @@ public class GamePanel extends JPanel implements Runnable{
 			}
 			
 		}
-		if (m_player.checkCollision(m_tab_pnj.get(1).m_x, m_tab_pnj.get(1).m_y, TILE_SIZE)) {
-			g2.drawString("Dialogue pnj 2", m_player.m_x, m_player.m_y - 10);
+		if (m_player.checkCollision(m_tab_pnj_1.get(1).m_x, m_tab_pnj_1.get(1).m_y, TILE_SIZE)) {
+			g2.drawString("PNJ bureau", m_player.m_x, m_player.m_y - 10);
 		}
-		if (m_player.checkCollision(m_tab_pnj.get(2).m_x, m_tab_pnj.get(2).m_y, TILE_SIZE)) {
-			g2.drawString("Dialogue pnj 3", m_player.m_x, m_player.m_y - 10);
+		if (m_player.checkCollision(m_tab_pnj_1.get(2).m_x, m_tab_pnj_1.get(2).m_y, TILE_SIZE)) {
+			g2.drawString("AMPHI M", m_player.m_x, m_player.m_y - 10);
+		}
+		
+		if (m_player.checkCollision(m_tab_pnj_2.get(1).m_x, m_tab_pnj_2.get(1).m_y, TILE_SIZE)) {
+			g2.drawString("Bienvenue au BDE", m_player.m_x, m_player.m_y - 10);
 		}
 	}
 	
