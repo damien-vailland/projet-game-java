876c581bcb36b1e200e50c907ea28244e20d24c9
Morneee <129755978+Morneee@users.noreply.github.com>
2024-05-29 09:46:49 +0200

    petit truc

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -250,8 +250,8 @@ public class GamePanel extends JPanel implements Runnable{
         int currentMonthIndex = (int) ((System.currentTimeMillis() - startTime) / monthDuration);
         if (currentMonth != months[currentMonthIndex]) {
         	currentMonth = months[currentMonthIndex];
-        	coeff_satisfaction=m_nb_teacher/m_nb_student;
-            m_player.updatePourcentageSatisfaction(coeff_satisfaction);
+        	coeff_satisfaction=m_nb_student/m_nb_teacher;
+            m_player.updatePourcentageSatisfaction(-coeff_satisfaction);
 			int x=-5;
         	//la barre de vie diminue plus vite lorsque la machine à café est cassée
         	if(m_tileM.breakCoffee()) {
@@ -411,7 +411,7 @@ public class GamePanel extends JPanel implements Runnable{
 			if (m_tileM.m_use && inventaire.contains(m_craie) ) {
 				inventaire.remove(m_craie);
 				m_player.updateScore(100);
-				m_player.updatePourcentageSatisfaction(20);
+				m_player.updatePourcentageSatisfaction(10);
 				m_quete1 = false;
 			}
 			
@@ -440,7 +440,7 @@ public class GamePanel extends JPanel implements Runnable{
 			if (m_tileM.m_use && inventaire.contains(m_clef) ) {
 				inventaire.remove(m_clef);
 				m_player.updateScore(100);
-				m_player.updatePourcentageSatisfaction(20);
+				m_player.updatePourcentageSatisfaction(10);
 				m_quete3 = false;
 			}
 		}
@@ -464,7 +464,7 @@ public class GamePanel extends JPanel implements Runnable{
 			if (m_tileM.m_use) {
 				m_quete2 = false;
 				m_player.updateScore(100);
-				m_player.updatePourcentageSatisfaction(20);
+				m_player.updatePourcentageSatisfaction(10);
 			}
 		}else {
 			m_pnj_mobile.get(0).pause = true;
