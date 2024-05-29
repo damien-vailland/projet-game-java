4b94746377224607d1478acb2efd53dabcb1aaaf
Morneee <129755978+Morneee@users.noreply.github.com>
2024-05-29 13:03:08 +0200

    dernier truc

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        long monthDuration = 6 * 1000;
+        long monthDuration = 180 * 1000;
         long startTime = System.currentTimeMillis();
         String[] months = {"Septembre", "Octobre", "Novembre", "Décembre", "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet"};
 
